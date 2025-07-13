c18ca1b3a52eabe2ff24915e0063f25bde790a23
georgiana-stoica <sgeo2001@gmail.com>
2025-07-13 19:01:26 +0300

    React course first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Menu/>
+            <Footer/>
+        </div>
+    );
+}
+
+function Header() {
+    //const style = {color: 'red', fontsize: '32px', textTransform: 'uppercase'};
+    const style = {};
+    return <header className='header'>
+        <h1>Fast React Pizza Co.</h1>
+    </header>
+}
+
+function Menu() {
+    return <main className='menu'>
+        <h2>Our Menu</h2>
+        <Pizza/>
+        <Pizza/>
+        <Pizza/>
+    </main>;
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+    // if ( hour >= openHour && hour <= closeHour )
+    //     alert("We're currently open!");
+    //
+
+    return <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open!</footer>;
+}
+
+function Pizza() {
+    return (<div>
+        <img src="../pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
+        <h3>Pizza Spinaci</h3>
+        <p>Tomato, mozarella, spianch, and ricotta cheese</p>
+    </div>);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App/>
+    </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
